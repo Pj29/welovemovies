21369015affdb44e2d4afc7eff84f82e482d3684
Peter Jones <pjxcvi@proton.me>
2023-10-31 01:10:21 -0400

    cors settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,3 @@
-if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -10,7 +9,20 @@ const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
-app.use(cors());
+// Define your allowed origins (your React app's domain)
+const allowedOrigins = ["https://welovemovies-client-du0e.onrender.com"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
